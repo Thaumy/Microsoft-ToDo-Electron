23b50249211620f3b6c52df1966b7567df95c848
Thaumy <thaumy@outlook.com>
2022-12-15 00:36:16 +0800

    add icon and prepare for nix-build

--- default.nix
+++ default.nix
@@ -1,14 +1,31 @@
-with import <nixpkgs> {};
+{ stdenv, pkgs, lib, fetchurl, makeDesktopItem }:
 
-let 
+stdenv.mkDerivation rec {
+  name = "microsoft-todo-electron";
+  version = "1.3.1";
+
+  phases = [ "unpackPhase" "installPhase" ];
+
+  desktopItem = makeDesktopItem rec {
+    name = "microsoft-todo-electron";
+    desktopName = "ToDo";
+    genericName = desktopName;
+    comment = "Microsoft ToDo wrapped in Electron";
+
+    exec = "${out}/${desktopName}";
+    icon = "${out}/icon.icns";
+
+    type = "Application";
+  };
+
   rpath = with pkgs; lib.makeLibraryPath [
-    nss
     atk
+    nss
+    nspr
     zlib
-    mesa
     glib
     gtk3
-    nspr
+    mesa
     cups
     dbus
     expat
@@ -17,43 +34,39 @@ let
     libpng
     libdrm
     systemd
-    freetype
     alsa-lib
-    fontconfig
+    freetype
     gdk-pixbuf
+    fontconfig
     xorg.libXi
     xorg.libX11
     xorg.libxcb
-    at-spi2-core
-    libxkbcommon
     xorg.libXext
     stdenv.cc.cc
-    xorg.libXfixes
+    at-spi2-core
+    libxkbcommon
     xorg.libXrandr
+    xorg.libXfixes
     xorg.libXcursor
-    xorg.libXdamage
     xorg.libXrender
+    xorg.libXdamage
     xorg.libXcomposite
   ];
 
   src = fetchurl {
-      url = "https://github.com/Thaumy/Microsoft-ToDo-Electron/releases/download/v1.3.0/microsoft-todo-electron-1.3.0.tar.gz";
-      sha256 = "1n3sbw2xc9dqz6drnmdbjvkx8zxls60xmjlmfs07zxhm2a2amlp5";
+    url = "https://github.com/Thaumy/Microsoft-ToDo-Electron/releases/download/v1.3.1/microsoft-todo-electron-1.3.1.tar.gz";
+    sha256 = "";
   };
-in 
-  stdenv.mkDerivation rec {
-    name = "microsoft-todo-electron";
+  
+  unpackPhase = ''
+    mkdir unzipped
+    tar -xvzf ${src} -C unzipped
+  '';
 
-    phases = [ "unpackPhase" "installPhase" ];
-    
-    unpackPhase = ''
-      mkdir unzipped
-      tar -xvzf ${src} -C unzipped
-    '';
-
-    installPhase = ''
-      cp -r unzipped/* $out
-      patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" --set-rpath ${rpath}:$out $out/microsoft-todo-electron
-      echo `ldd $out/microsoft-todo-electron | grep "not found"` # echo for debug.
-    '';
-  }
+  installPhase = ''
+    cp -r unzipped/* $out
+    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" --set-rpath ${rpath}:$out $out/microsoft-todo-electron
+    ln -s ${desktopItem}/share/applications/* $out/share/applications
+    echo `ldd $out/microsoft-todo-electron | grep "not found"` # echo for debug.
+  '';
+}
