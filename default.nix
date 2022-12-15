6e97655eb799b21bbfb9a79435ad0a4a4cde8243
Thaumy <thaumy@outlook.com>
2022-12-15 23:44:05 +0800

    change icon to png

--- default.nix
+++ default.nix
@@ -1,19 +1,16 @@
 { stdenv, pkgs, lib, fetchurl, makeDesktopItem }:
 
-stdenv.mkDerivation rec {
-  name = "microsoft-todo-electron";
-  version = "1.3.1";
-
-  phases = [ "unpackPhase" "installPhase" ];
+let
+  appBinName = "microsoft-todo-electron";
 
   desktopItem = makeDesktopItem rec {
-    name = "microsoft-todo-electron";
+    name = appBinName;
     desktopName = "ToDo";
     genericName = desktopName;
     comment = "Microsoft ToDo wrapped in Electron";
 
-    exec = "${out}/${desktopName}";
-    icon = "${out}/icon.icns";
+    exec = name;
+    icon = name;
 
     type = "Application";
   };
@@ -57,7 +54,15 @@ stdenv.mkDerivation rec {
     url = "https://github.com/Thaumy/Microsoft-ToDo-Electron/releases/download/v1.3.1/microsoft-todo-electron-1.3.1.tar.gz";
     sha256 = "0w51cbkkwc392pqnz2p98xin84560lpqjpgp0sgx258b01gj230i";
   };
-  
+
+in
+
+stdenv.mkDerivation rec {
+  pname = "microsoft-todo-electron";
+  version = "1.3.1";
+
+  phases = [ "unpackPhase" "installPhase" ];
+
   unpackPhase = ''
     mkdir unzipped
     tar -xvzf ${src} -C unzipped
@@ -65,8 +70,31 @@ stdenv.mkDerivation rec {
 
   installPhase = ''
     cp -r unzipped/* $out
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" --set-rpath ${rpath}:$out $out/microsoft-todo-electron
+
+    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" --set-rpath ${rpath}:$out $out/${appBinName}
+    # echo for debug
+    echo `ldd $out/${appBinName} | grep "not found"`
+
+    mkdir -p $out/bin
+    ln -s $out/${appBinName} $out/bin/${appBinName}
+
+    # .icns
+    mkdir -p $out/share/icons
+    ln -s $out/icon.icns $out/share/icons/${appBinName}
+
+    # .desktop
+    mkdir -p $out/share/applications
     ln -s ${desktopItem}/share/applications/* $out/share/applications
-    echo `ldd $out/microsoft-todo-electron | grep "not found"` # echo for debug.
+
+    # echo for debug
+    echo "App was installed in $out"
   '';
+
+  meta = {
+    description = "Microsoft ToDo wrapped in Electron";
+    homepage = "https://github.com/Thaumy/Microsoft-ToDo-Electron";
+    license = lib.licenses.mit;
+    maintainers = [ "thaumy" ];
+    platforms = lib.platforms.linux;
+  };
 }
